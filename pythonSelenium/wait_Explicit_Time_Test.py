05b4df49e4f4cce10ed149e066e824f9495b88b8
Moises Pirela <m.pirela@gmail.com>
2022-05-13 18:39:53 -0300

    Wait Explicit Time

--- pythonSelenium/wait_Explicit_Time_Test.py
+++ pythonSelenium/wait_Explicit_Time_Test.py
@@ -34,13 +34,18 @@ driver.find_element(By.CSS_SELECTOR, value="img[alt='Cart']").click()
 # Para seleccionar el botón de Proceed to Checkout
 driver.find_element(By.XPATH, value="//button[text()='PROCEED TO CHECKOUT']").click()
 
+
+
 #Explicit Wait time. Para esperar que cargue el checkout y luego introducir el código
 wait = WebDriverWait(driver, 5)
 wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME, 'promoCode')))
 
+# Para Extraer el monto antes del descuento
+previous_disc = float(driver.find_element(By.CLASS_NAME, value="totAmt").text)
+print(previous_disc)
+
 # para ingresar el cupón descuento
 driver.find_element(By.XPATH, value="//input[@class='promoCode']").send_keys("rahulshettyacademy")
-
 
 # Para tomar el nombre de los elementos en la tabla
 vegetables = driver.find_elements(By.CSS_SELECTOR, value="p.product-name")
@@ -54,7 +59,29 @@ assert all_items == list2
 # Para presionar promo button
 driver.find_element(By.CSS_SELECTOR, value='.promoBtn').click()
 
-# Para saber que la promoción fue aplicada
-#applied = driver.find_element(By.XPATH, value="//span[@class='promoInfo']").text
+driver.implicitly_wait(5)
 
-#assert "applied" in applied
+# Para saber que la promoción fue aplicada
+applied = driver.find_element(By.XPATH, value="//span[@class='promoInfo']").text
+
+assert "applied" in applied
+
+
+# Para extraer total después del descuento
+after_disc = float(driver.find_element(By.CLASS_NAME, value="discountAmt").text)
+print(after_disc)
+assert previous_disc > after_disc
+
+# Para extraer el precio de cada producto
+prices = driver.find_elements(By.CSS_SELECTOR,value="tr td:nth-child(5) p[class='amount']")
+
+#total_cost= []
+#for price in prices:
+#    total_cost.append(int(price.text))
+
+total_cost = 0
+for price in prices:
+    total_cost += int(price.text)
+
+print(total_cost)
+assert total_cost == previous_disc
